090ad4fd1f9c9458ff9cf300c9e3247d6ce46259
adverley <andreas.verleysen@gmail.com>
2020-03-31 13:00:10 +0200

    add setup stuff

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='MyCommons',
+    version='0.1.0',
+    author='Andreas Verleysen',
+    author_email='andreas.verleysen@gmail.com',
+    packages=['commons'],
+    license='LICENSE.txt',
+    description='Functionality I keep on rewriting',
+    long_description=open('README.md').read(),
+    install_requires=requirements,  # this is required because we want to pip install from git which will not read the requirements.txt file
+)
